d3fdc726ef36a899317ce8ebbab30b825ac7acd6
juliocsar13 <jc_redcsar_15@hotmail.com>
2015-08-16 01:27:26 -0500

    implementacion de TAB HOST con fragmentos :v terimado  D:

--- app/src/main/java/app/akitaxi_usuario/julio/my_tabhost/Tab_September.java
+++ app/src/main/java/app/akitaxi_usuario/julio/my_tabhost/Tab_September.java
@@ -11,6 +11,6 @@ public class Tab_September extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tab_september);
+        setContentView(R.layout.customitem);
     }
 }
